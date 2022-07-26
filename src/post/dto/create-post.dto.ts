f111f192fb2a3e29898708468bf15a8fc7be1ef0
GR7ME <lsinjali40@gmail.com>
2022-07-27 00:20:03 +0545

    fixed leaked code

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreatePostDto {
 
+    @ApiProperty({description:"Title of a article"})
     @IsString()
     @IsNotEmpty()
     title: string
 
+    @ApiProperty({description:"Description of a article"})
     @IsString()
     description?: string
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
     
+    @ApiProperty({description:"Email of user"})
     @IsString()
     @IsEmail()
     @IsNotEmpty()
     email: string
 
+
+    @ApiProperty({description:"Password of user"})
     @IsString()
     @IsNotEmpty()
     password: string
